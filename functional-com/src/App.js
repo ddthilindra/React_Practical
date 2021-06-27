4e790d9df0ab504b3608f91ff87c4f06cc30bf1c
ddthilindra <ddthilindra@gmail.com>
2021-06-28 00:52:18 +0530

    Props and State

--- functional-com/src/App.js
+++ functional-com/src/App.js
@@ -1,15 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import Greet from './component/Greet';
 import Welcome from './component/Welcome';
 import Hello from './component/Hello';
+import Greet from './component/Greet';
+import Message from './component/Message';
 
 function App() {
   return (
     <div className="App">
-      {/* <Greet /> */}
-      {/* <Welcome /> */}
-      <Hello />
+      <Message />
+      {/* <Greet name="Bruce" heroName="Batman" >
+        <p>This is children props</p>
+      </Greet>
+      <Greet name="Clark" heroName="Suerman">
+        <button>Action</button>
+      </Greet>
+      <Greet name="Diana" heroName="Wonder Women"/>
+      <Welcome name="Bruce" heroName="Batman"></Welcome>
+      <Welcome name="Clark" heroName="Suerman"></Welcome>
+      <Welcome name="Diana" heroName="Wonder Women"></Welcome> */}
     </div>
   );
 }
--- functional-com/src/component/Greet.js
+++ functional-com/src/component/Greet.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
-function Greet() {
-    return <h1>Hello ddthilindra.!</h1>
+function Greet = props => {
+    console.log(props)
+    return (
+        <div>
+            <h1>Hello {props.name} a.k.a {props.heroName}</h1>
+            {props.children}
+        </div>
+    )
 }
 
 export default Greet
